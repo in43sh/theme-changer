0724aef50b6d4fd8ac07328720250aa7589f3490
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-04 12:18:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,19 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor() {
+    super();
+
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    };
+  }
 
   // updateColor
+  
 
   // updateSize
 
